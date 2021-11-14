c79914d4c0d42161500bd1642068f052712057f4
Iacopo02 <91663843+Iacopo02@users.noreply.github.com>
2021-11-14 15:51:00 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+
 using namespace std;
 
-int main() {
+int main() 
+{
+   int n, i=1, b=1, a=0, c;
+   cin>>n;
+   
+   
+   if(n>=2)
+   {
+      cout<<b<<endl;
+      for(i=2; i<=n; i++)
+      {
+         
+         c=a+b;
+         cout<<c<<endl;
+         a=b;
+         b=c;
+         
+      }
+      
+      
+      
+   }
+   else
+   {
+      cout<<"errore"<<endl;
+   }
+        
    return 0;
 }
